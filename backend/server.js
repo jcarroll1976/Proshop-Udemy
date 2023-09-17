03783c11ed79869fe127fd16b5a0972fdc5897d6
Joshua Carroll <carrolljosh1976@gmail.com>
2023-09-17 13:58:46 -0400

    started to setup database and database connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
-const port = 5000;
+const port = process.env.PORT;
 
+connectDB(); //Connect to MongoDB
 
 const app = express();
 
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useEffect,useState } from 'react';
+import axios from "axios";
 import {Row,Col} from "react-bootstrap";
 import Product from '../components/Product';
-import products from "../products";
+
 
 function HomeScreen() {
+  const [products,setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const {data} = await axios.get("/api/products");
+      setProducts(data);
+    };
+
+    fetchProducts();
+  },[])
+
   return (
     <div>
         <h1>Latest Products</h1>
