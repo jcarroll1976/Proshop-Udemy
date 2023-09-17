7505dc386ccf3995a9807745e9a1449d64546572
Joshua Carroll <carrolljosh1976@gmail.com>
2023-09-16 20:43:37 -0400

    Implemented routing

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Row,Col} from "react-bootstrap";
+import Product from '../components/Product';
 import products from "../products";
 
 function HomeScreen() {
@@ -7,9 +8,9 @@ function HomeScreen() {
     <div>
         <h1>Latest Products</h1>
         <Row>
-            {products.map(() => (
-                <Col sm={12} md={6} lg={4} xl={3}>
-                    
+            {products.map((product) => (
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                    <Product product={product} />
                 </Col>
             ))}
         </Row>
