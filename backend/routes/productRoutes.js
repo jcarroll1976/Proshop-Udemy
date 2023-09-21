e1b39fb2d00eb33ac4440d1ffa1058bd18f1799d
Joshua Carroll <carrolljosh1976@gmail.com>
2023-09-20 22:37:12 -0400

    added async handler and error handler

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,15 +1,24 @@
 import express from "express";
 
 const router = express.Router();
-import products from "../data/products.js";
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../models/productModel.js";
+//import products from "../data/products.js";
 
-router.get("/", (req,res) => {
+router.get("/", asyncHandler(async (req,res) => {
+    const products = await Product.find({});
     res.json(products);
-})
+}));
 
-router.get("/:id", (req,res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
+router.get("/:id", asyncHandler(async (req,res) => {
+    const product = await Product.findById(req.params.id);
+    if(product) {
+       return res.json(product);
+    } else {
+        res.status(404);
+        throw new Error("Resource not found");  
+    }
 })
+);
 
-export default router
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
 import products from "./data/products.js";
-
+import { notFound,errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 const port = process.env.PORT;
 
@@ -17,6 +17,7 @@ app.get("/", (req,res) => {
 
 app.use('/api/products', productRoutes);
 
-
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
