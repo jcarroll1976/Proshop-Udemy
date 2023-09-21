e9b5d9c538cc3f9179c7f58c0aa5c2abc54bd1fb
Joshua Carroll <carrolljosh1976@gmail.com>
2023-09-20 21:21:25 -0400

    added product routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
 import products from "./data/products.js";
+
+import productRoutes from "./routes/productRoutes.js";
 const port = process.env.PORT;
 
 connectDB(); //Connect to MongoDB
@@ -13,13 +15,8 @@ app.get("/", (req,res) => {
     res.send("API is running...");
 })
 
-app.get("/api/products", (req,res) => {
-    res.json(products);
-})
+app.use('/api/products', productRoutes);
 
-app.get("/api/products/:id", (req,res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-})
+
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
