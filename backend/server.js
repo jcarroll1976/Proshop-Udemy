6d50af68010def3b91b96cb00b30ab3678b2f715
Joshua Carroll <carrolljosh1976@gmail.com>
2023-09-25 22:12:14 -0400

    started implementing login functionality

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
 import connectDB from "./config/db.js";
 import products from "./data/products.js";
@@ -12,8 +13,12 @@ connectDB(); //Connect to MongoDB
 
 const app = express();
 
+//Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+//Cookie parser middleware
+app.use(cookieParser());
 
 app.get("/", (req,res) => {
     res.send("API is running...");
