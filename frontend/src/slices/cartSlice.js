0e698c1f500f63ff3d76a3500e81f3c35acf0a94
Joshua Carroll <carrolljosh1976@gmail.com>
2023-09-27 21:54:11 -0400

    finished user registration page and started to implement saving shipping address in state

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import { updateCart } from "../utils/cartUtils";
 
 const initialState = localStorage.getItem("cart") ? JSON.parse
-(localStorage.getItem("cart")) : {cartItems: []};
+(localStorage.getItem("cart")) : {cartItems: [], shippingAddress: {}, paymentMethod: "PayPal"};
 
 
 const cartSlice = createSlice({
@@ -26,6 +26,10 @@ const cartSlice = createSlice({
         removeFromCart: (state,action) => {
             state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
 
+            return updateCart(state);
+        },
+        saveShippingAddress: (state,action) => {
+            state.shippingAddress = action.payload;
             return updateCart(state);
         }
     },
